48b1bc4ec534a185fa278d7183729071513eaef1
Farhan <farhanmnavas@gmail.com>
2024-01-08 17:11:59 +0800

    Start creation of header

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-} from "@/components/ui/navigation-menu"
-
-
+import Header from "./components/Header"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink>Link</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
+      <div>
+        <Header/>
+      </div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import Container from "./ui/container"
+
+const header = () => {
+    return (
+        <header className="sm:flex sm:justify-between py-3 px-4 border-b">
+            <Container>
+                <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center
+                justify-between w-full">
+                    <div className="flex items-center">
+                        Nigger
+                    </div>
+                </div>
+            </Container>
+        </header>
+    )
+}
+
+export default header 
